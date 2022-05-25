94538f711691230232e5ca7a5b44a3cd0a5cf5f8
kvy98 <dinhkhanhvy85@gmail.com>
2022-05-25 10:50:39 +0700

    init

---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="login">Google signin</button>
+</template>
+<script>
+export default {
+  methods: {
+    login() {
+      this.$auth.loginWith('google', { params: { prompt: 'select_account' } })
+    },
+  },
+}
+</script>
